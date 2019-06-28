962ef5e46790c194533d8ac49001cd349ddae548
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2019-06-28 17:46:06 +0700

    add standard library

---
+++ src/BuildingBlocks/EventBus/EventBus/Abstractions/IIntegrationEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+using BuildingBlocks.EventBus.Events;
+
+namespace BuildingBlocks.EventBus.Abstractions
+{
+    public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+       where TIntegrationEvent : IntegrationEvent
+    {
+        Task Handle(TIntegrationEvent @event);
+    }
+
+    public interface IIntegrationEventHandler
+    {
+    }
+}
---
+++ src/BuildingBlocks/EventBus/IntegrationEventLogEF/Services/IIntegrationEventLogService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Data.Common;
+using System.Threading.Tasks;
+using BuildingBlocks.EventBus.Events;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace BuildingBlocks.IntegrationEventLogEF.Services
+{
+    public interface IIntegrationEventLogService
+    {
+        Task SaveEventAsync(IntegrationEvent @event, IDbContextTransaction transaction);
+        Task MarkEventAsPublishedAsync(Guid eventId);
+        Task MarkEventAsInProgressAsync(Guid eventId);
+        Task MarkEventAsFailedAsync(Guid eventId);
+    }
+}

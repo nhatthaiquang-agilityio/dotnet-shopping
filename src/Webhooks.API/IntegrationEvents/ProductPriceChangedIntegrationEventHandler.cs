ede56b85b4c3ec36edf29d1b62727baad27c64b6
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2019-07-23 16:46:12 +0700

    Add Webhook API service

---
+++ src/Webhooks.API/IntegrationEvents/ProductPriceChangedIntegrationEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using BuildingBlocks.EventBus.Abstractions;
+using System.Threading.Tasks;
+
+namespace Webhooks.API.IntegrationEvents
+{
+    public class ProductPriceChangedIntegrationEventHandler : IIntegrationEventHandler<ProductPriceChangedIntegrationEvent>
+    {
+        public async Task Handle(ProductPriceChangedIntegrationEvent @event)
+        {
+            await Task.Delay(1);
+        }
+    }
+}

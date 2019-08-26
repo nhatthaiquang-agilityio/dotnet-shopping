a0830306438e293aa344bd58eccfc81178abeda9
Nhat Thai <nhat.thaiquang@asnet.com.vn>
2019-08-26 17:01:55 +0700

    Add Testing

---
+++ examples/dotnet-express-mapper/Testing/BooksControllerTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using dotnet_express_mapper;
+using dotnet_express_mapper.Models;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Testing
+{
+    // Integration Test
+    public class BooksControllerTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    {
+        private readonly HttpClient _client;
+
+        public BooksControllerTests(CustomWebApplicationFactory<Startup> factory)
+        {
+            _client = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task TestGetListBooks()
+        {
+            var httpResponse = await _client.GetAsync("http://localhost/api/books");
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Deserialize and examine results.
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var books = JsonConvert.DeserializeObject<IEnumerable<BookViewModel>>(stringResponse);
+
+            Assert.Contains(books, p => p.Author == "Stephen Ken");
+            Assert.Contains(books, p => p.BookName == "Basic C#");
+        }
+
+        [Fact]
+        public async Task TestCreateBook()
+        {
+            var bookModel = new BookViewModel
+            {
+                AuthorId = 2,
+                Price = (decimal)43.45,
+                BookName = "Advance C#"
+            };
+
+            HttpContent content = new StringContent(
+                JsonConvert.SerializeObject(bookModel),
+                Encoding.UTF8, "application/json");
+            var httpResponse = await _client.PostAsync("http://localhost/api/books", content);
+
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Deserialize and examine results.
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var book = JsonConvert.DeserializeObject<Book>(stringResponse);
+
+            Assert.Equal(43.45, (double)book.Price);
+            Assert.Equal("Advance C#", book.BookName);
+            Assert.NotNull(book.Id);
+        }
+
+        [Fact]
+        public async Task TestGetBook()
+        {
+            var httpResponse = await _client.GetAsync("http://localhost/api/books/1");
+
+            // Must be successful.
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Deserialize results.
+            var contentResponse = await httpResponse.Content.ReadAsStringAsync();
+            var bookObj = JsonConvert.DeserializeObject<BookViewModel>(contentResponse);
+
+            Assert.Equal(43.45, (double)bookObj.Price);
+            Assert.Contains("Basic C#", bookObj.BookName);
+        }
+    }
+}
---
+++ examples/dotnet-express-mapper/Testing/SeedData.cs
@@ -0,0 +1,58 @@
+﻿using dotnet_express_mapper.Data;
+using dotnet_express_mapper.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    public class SeedData
+    {
+        public void InitData(AppDbContext context)
+        {
+            CreateBooks(context).Wait();
+        }
+
+        private async Task CreateBooks(AppDbContext context)
+        {
+			await context.Authors.AddRangeAsync(new List<Author> {
+				new Author { FirstName = "Stephen", LastName = "Ken" },
+				new Author { FirstName = "David", LastName = "Coup" },
+				new Author { FirstName = "Tom", LastName = "Hank" }
+			});
+
+			var category = new Category
+            {
+                CategoryName = "Network"
+            };
+
+            var category1 = new Category
+            {
+                CategoryName = "Programming"
+            };
+            await context.Categories.AddRangeAsync(new List<Category> { category, category1 });
+            await context.SaveChangesAsync();
+
+            var book = new Book
+            {
+                Price = (decimal)43.45,
+                BookName = "Basic C#",
+                AuthorId = 1
+            };
+
+            book.BookCategories = new List<BookCategory>
+            {
+                new BookCategory
+                {
+                    Category = category1,
+                    Book = book
+                }
+            };
+
+            //Now add this book, with all its relationships, to the database
+            await context.Books.AddRangeAsync(new List<Book> { book });
+			//await context.BookCategories.AddRangeAsync(book.BookCategories);
+			await context.SaveChangesAsync();
+
+        }
+    }
+}
